6b2bed492a675e3331c8dae3a09c93139138cc58
PREMINDA <premindabandara0777@gmail.com>
2021-01-28 02:37:41 +0530

    Controlled Inputs

--- src/Creat.js
+++ src/Creat.js
@@ -1,9 +1,21 @@
 const Create = () => {
-    return ( 
-        <div className="create">
-            <h2>Add a New Blog</h2>
-        </div>
-     );
-}
- 
+  return (
+    <div className="create">
+      <h2>Add a New Blog</h2>
+      <form>
+        <label>Blog title</label>
+        <input type="text" required />
+        <label>Blog</label>
+        <textarea required />
+        <label>Blog author:</label>
+        <select>
+          <option value="Mario">Mario</option>
+          <option value="Yoshi">Yoshi</option>
+        </select>
+      </form>
+      <button>Add Blog</button>
+    </div>
+  );
+};
+
 export default Create;
